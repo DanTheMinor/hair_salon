0fcc957e792b6d014c956ccf69992155dec6ee99
Dan <student@epicodus.com>
2015-05-08 09:40:38 -0700

    pass first of clients

--- lib/client.rb
+++ lib/client.rb
@@ -1,11 +1,21 @@
-require('spec_helper')
+class Client
+  attr_reader(:name, :stylist_id)
 
-describe(Client) do
-  describe(".all") do
-    it("is empty at first") do
-      expect(Client.all()).to(eq([]))
-    end
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+    @stylist_id = attributes.fetch(:stylist_id).to_i
   end
-  
+
+  define_singleton_method(:all) do
+    db_clients = DB.exec("SELECT * FROM clients;")
+    clients = []
+    db_clients.each() do |client|
+      name = task.fetch("name")
+      stylist_id = task.fetch("stylist_id").to_i
+      clients.push(Client.new({:name => name, :stylist_id => stylist_id}))
+    end
+    return clients
+  end
+
 
 end
