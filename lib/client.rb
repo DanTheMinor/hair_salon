5f3d95555348ec593814861448c644a32143b812
Dan <student@epicodus.com>
2015-05-08 09:47:24 -0700

    pass first of clients

--- lib/client.rb
+++ lib/client.rb
@@ -17,5 +17,8 @@ class Client
     return clients
   end
 
+  define_method(:==) do |another_client|
+    self.name().==(another_client.name()).&(self.stylist_id().==(another_client.stylist_id()))
+  end
 
 end
