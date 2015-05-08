c7e261016fe3f35f9855d30168e93daaa2325d81
Dan <student@epicodus.com>
2015-05-08 11:28:49 -0700

    create stylists and new stylist page, create clients and new client page. write spec for viewing stylists and adding new stylist, specs pass. write spec for viewing clients.

---
+++ app.rb
@@ -0,0 +1,35 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/client')
+require('./lib/stylist')
+also_reload('lib/**/*.rb')
+require('pg')
+require('pry')
+
+DB = PG.connect({:dbname => "hair_salon"})
+
+get('/') do
+  erb(:index)
+end
+
+get('/stylists') do
+  @all_stylists = Stylist.all()
+  erb(:stylists)
+end
+
+post('/new_stylist') do
+  stylist_name = params.fetch("stylist_name")
+  Stylist.new({:name => stylist_name, :id => nil}).save()
+  @all_stylists = Stylist.all()
+  erb(:stylists)
+end
+
+get('/single_stylist/:id') do
+  @stylist_list = Stylist.find(params.fetch("id").to_i())
+  erb(:single_stylist)
+end
+
+get('/clients') do
+  @all_clients = Client.all()
+  erb(:clients)
+end
---
+++ spec/salon_integration_spec.rb
@@ -0,0 +1,35 @@
+require('capybara/rspec')
+require('./app')
+require('spec_helper')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+require('pry')
+
+describe('The stylist path', {:type => :feature}) do
+  it("displays a list of all stylists") do
+    test_stylist = Stylist.new({name: 'Bob', id: 1})
+    test_stylist.save()
+    visit('/')
+    click_link('View a list of stylists')
+    expect(page).to have_content(test_stylist.name())
+  end
+end
+
+describe('The stylist creation path', {:type => :feature}) do
+  it("provides a form for creating a new stylist and when form is submitted, saves new stylist to database") do
+    visit('/stylists')
+    fill_in('stylist_name', :with => 'Bob')
+    click_button("Add new stylist")
+    expect(page).to have_content('Bob')
+  end
+end
+
+describe('The client path', {:type => :feature}) do
+  it("displays a list of all clients") do
+    test_client = Client.new({name: 'Dan', id: 1})
+    test_client.save()
+    visit('/')
+    click_link('View a list of clients')
+    expect(page).to have_content('Dan')
+  end
+end
