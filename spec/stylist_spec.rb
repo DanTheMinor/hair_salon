a177ba57603b974ea7600e0eb497cc2c25081762
Dan <student@epicodus.com>
2015-05-08 10:09:04 -0700

    write spec for save method, method passes.

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -8,7 +8,7 @@ describe(Stylist) do
   end
 
   describe("#save") do
-    it("Lets you save a stylist to the database") do
+    it("Lets you save a stylist to the database and assigns them an ID based on their db location") do
         test_stylist = Stylist.new({:name => 'Bob', :id => nil})
         test_stylist.save()
         expect(Stylist.all()).to(eq([test_stylist]))
